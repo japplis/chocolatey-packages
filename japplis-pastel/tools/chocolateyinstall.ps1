764116d3095784860896fabdcc9962e8fe4f9433
Anthony Goubard <anthony.goubard@japplis.com>
2023-11-06 11:58:08 +0100

    Japplis Pastel version 1.1

--- japplis-pastel/tools/chocolateyinstall.ps1
+++ japplis-pastel/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Japplis Pastel*'
-  checksum      = 'a2a3b9ada69a4b97f5226a6f4109c505d279b8ee0c0de4c966ce68d6f1cf9b29'
+  checksum      = 'f878c1a16037ebb0b13a80541dce3f5ae073b3439fd1f2a31fb009fe3be0a7e9'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
