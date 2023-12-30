86423f15fabf7c5dbe867bf62b7543e46f7c155d
Anthony Goubard <anthony.goubard@japplis.com>
2023-12-30 11:05:16 +0100

    Ant Commander Pro version 4.4

--- ant-commander-pro/tools/chocolateyinstall.ps1
+++ ant-commander-pro/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Ant Commander Pro*'
-  checksum      = 'c1ecf7a4d7b081ffe5fcb28fefbb12e88f02d60652389ef691a390cd31d6dbe8'
+  checksum      = 'ddfe01a39206affad33b11680e9e2b40ed0b12f6ab3bd5db68b7f75cac2d17d9'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
