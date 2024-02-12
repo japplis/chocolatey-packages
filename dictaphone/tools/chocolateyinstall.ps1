a4e814bc929d75b7cfacb66e4f9c66d699814d58
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-12 11:43:21 +0100

    Dictaphone version 1.1

--- dictaphone/tools/chocolateyinstall.ps1
+++ dictaphone/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Dictaphone*'
-  checksum      = '3fce236c62e83a797cbf6c1bf1c307c207ab9c86bfa36286b6ceb424cc5a83b1'
+  checksum      = '883f20f91fbfe0265b307ac50481adb086f337f8aca38a4972fe51c96e965a58'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
