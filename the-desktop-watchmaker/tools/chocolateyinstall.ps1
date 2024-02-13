d36d95db8b2b3d9e0deeec5c3a9a3451ccddb371
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-13 10:48:52 +0100

    The Desktop Watchmaker version 3.2

--- the-desktop-watchmaker/tools/chocolateyinstall.ps1
+++ the-desktop-watchmaker/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'The Desktop Watchmaker*'
-  checksum      = '9bf2d03aec7cbfec561a69b01df308192507c5b4ab6ef26b93319c2672537016'
+  checksum      = '87270c0e2181a4ec86a066f37f9301518b60ef8c2a81fb22b6d3c948e6a51b03'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
