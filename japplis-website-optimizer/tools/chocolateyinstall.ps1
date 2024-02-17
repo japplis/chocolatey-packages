d6372cec172ec3e14c442abd71596fdc17051d7e
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-17 11:49:58 +0100

    Japplis Website Optimizer version 4.2

--- japplis-website-optimizer/tools/chocolateyinstall.ps1
+++ japplis-website-optimizer/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Japplis Website Optimizer*'
-  checksum      = '77663fd0bcf4ebe1ded7b6d02ce3f9a53d6f6114d7ca3412819df65bf3d14884'
+  checksum      = 'b0ccc9329d491df5e66e55ee39d2ce20f549d51ac0d956be3ad45ec7ad005983'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
