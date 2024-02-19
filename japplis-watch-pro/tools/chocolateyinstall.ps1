1b4311059da3e97f39f4e1863fd36f0229398dbc
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-19 09:50:31 +0100

    Japplis Watch Pro version 1.8

--- japplis-watch-pro/tools/chocolateyinstall.ps1
+++ japplis-watch-pro/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Japplis Watch Pro*'
-  checksum      = '3c2e4be1cfd7ee78c722488687c99e00a1e73e084f0bdf85236bd1a31b411afc'
+  checksum      = 'b031db6a7f5bbf9c83c2926f0602be21402f36d5ad1f8c79de4fb688b89760ad'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
