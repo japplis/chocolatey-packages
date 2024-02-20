3da9c702e1ba4b023677996323e999af74993eb8
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-20 11:21:17 +0100

    Sheet Viewer version 1.1

--- sheet-viewer/tools/chocolateyinstall.ps1
+++ sheet-viewer/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Sheet Viewer*'
-  checksum      = '93e984eea356b76a4d98dec7b97de86ea0f3b3c05dc5d0936ab0377f7acfb6e9'
+  checksum      = 'b5134c39dd8d8a7cabefe3be2d1a21708c90f6670943152fe00e479a2bd826bd'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
