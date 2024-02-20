baff24e186579197248364e380f103656bc763fe
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-20 11:07:18 +0100

    Sheet Stats version 1.1

--- sheet-stats/tools/chocolateyinstall.ps1
+++ sheet-stats/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Sheet Stats*'
-  checksum      = '8f46ca5f4eeacabf0fafd4fc4fd30c9c3a2e8b3460bac5389cb8e9999ddfc235'
+  checksum      = '99d24a4da4bb88698e9b22fba29614b928578974a2c59b6e839f3338368c3e92'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
