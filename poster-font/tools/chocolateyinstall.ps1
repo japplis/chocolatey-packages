7761c8e27a614d5cbb30072595e20c981d6c9a78
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-21 09:49:17 +0100

    Poster Font version 1.1

--- poster-font/tools/chocolateyinstall.ps1
+++ poster-font/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Poster Font*'
-  checksum      = '455042718e187dfb3bc74451236ef325c6e1a02637ea7b116bd5b377a4105c5f'
+  checksum      = '9ce0b0e6bb61f6075b5a17b9102431bf452481b623a32e1eb1508e4bc04a034a'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
