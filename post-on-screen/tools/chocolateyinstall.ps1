f231e8aa931abe2cb7caf849537f3e138a601e69
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-22 13:53:17 +0100

    Post On Screen version 1.2

--- post-on-screen/tools/chocolateyinstall.ps1
+++ post-on-screen/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Post On Screen*'
-  checksum      = '6e22fd54b3e4545a003f5d8430cba8cad4aaa7f0b445846434a68683369a86e1'
+  checksum      = '1fd12a7d580a608bdaaf601dd280dfc59da46fd57952194c3749a134eef310e1'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
