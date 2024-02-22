3cac43433d4bab1021fac5a8429fe34d1fd91b0f
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-22 16:04:54 +0100

    Control Dashboard version 1.1

--- control-dashboard/tools/chocolateyinstall.ps1
+++ control-dashboard/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Control Dashboard*'
-  checksum      = '4fa77ca1bb8c5714f4d56c702d06db06c8e589cfcf8b7a1680a51836b19259a5'
+  checksum      = 'c4658b9944600b5a602035d5b51c30fd24f05a3766eeda939f05e0d07823ef5a'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
