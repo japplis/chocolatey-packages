9fffe9e1765e66f5479ed8c968e1d923bc8dee17
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-22 13:43:10 +0100

    Screen Highlighter Pro version 1.1

--- screen-highlighter-pro/tools/chocolateyinstall.ps1
+++ screen-highlighter-pro/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Screen Highlighter Pro*'
-  checksum      = 'b60bf0ee7a085475dd9bf37a67c3cdae02fafe512df1d255328716ad02bfe049'
+  checksum      = '9a2fa89505a35bbd6b7356a1900589e0a9976ebe400e41946cfdd0de140a830c'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
