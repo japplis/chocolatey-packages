752c54b1f80e789f2c4d35e22e809454157a8699
Anthony Goubard <anthony.goubard@japplis.com>
2024-02-24 15:49:03 +0100

    JLearnIt version 6.6

--- jlearnit/tools/chocolateyinstall.ps1
+++ jlearnit/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'JLearnIt*'
-  checksum      = '399c186be01bc45a26310bea57f618f15f52c05a17313329432c833eca1df4d0'
+  checksum      = '926bd9099b8166084835636bce1021a3eec05fbd9abe41a7d1ac0420166f006b'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
