ba9a557fd2f074dbb19ab8c0dca908729b89da9f
Anthony Goubard <anthony.goubard@japplis.com>
2024-04-17 15:25:18 +0200

    Ant Commander Personal version 4.8

--- ant-commander-personal/tools/chocolateyinstall.ps1
+++ ant-commander-personal/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Ant Commander Personal*'
-  checksum      = 'b7437cd8626f29a6433e971cd26cf155763640248326280ecfc278304ec5630b'
+  checksum      = '9e7191e80daa397f0f580f156d57d3c12712a036424fd5acf4d4676994c817a9'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
