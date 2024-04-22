1a2cc10f27fa53ffec151075aa0bc909bdfdbdf8
Anthony Goubard <anthony.goubard@japplis.com>
2024-04-22 16:22:45 +0200

    Treecell version 1.2

--- treecell/tools/chocolateyinstall.ps1
+++ treecell/tools/chocolateyinstall.ps1
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   file          = $fileLocation
   softwareName  = 'Treecell*'
-  checksum      = '481b3030d8dec02979f9fdfc4ece464a9c996ce6c6d6c660664d42ba5ea3059c'
+  checksum      = '72ffa9324ac121982e3679aaa7d0924c9b4d00271cc8d3263bf18e70e5179a4e'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0,3010,1641)
