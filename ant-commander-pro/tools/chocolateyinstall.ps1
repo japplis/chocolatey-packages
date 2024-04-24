c307cb4c2f6d63fd0c1b6c7cb3a5276941b656f7
Anthony Goubard <anthony.goubard@japplis.com>
2024-04-24 13:12:39 +0200

    update ant commander pro 4.8

--- ant-commander-pro/tools/chocolateyinstall.ps1
+++ ant-commander-pro/tools/chocolateyinstall.ps1
@@ -3,15 +3,15 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'AntCommanderPro.exe'
 
 $packageArgs = @{
-  packageName   = 'ant-commander-pro'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Ant Commander Pro*'
   checksum      = '29956de85a874ac5eff903219bcb60b83c4f757c0e142a9e1343d1ce55d7c781'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
