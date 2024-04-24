6d176e93dc4050cc1b6d88c65e7fec1a9ec91020
Anthony Goubard <anthony.goubard@japplis.com>
2024-04-24 13:15:58 +0200

    update VR Applet Runner Pro 2.11

--- applet-runner-pro/tools/chocolateyinstall.ps1
+++ applet-runner-pro/tools/chocolateyinstall.ps1
@@ -3,15 +3,15 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'AppletRunnerPro.exe'
 
 $packageArgs = @{
-  packageName   = 'applet-runner-pro'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Applet Runner Pro*'
   checksum      = '7c6d3c63ebf1094506e4e8af6052cbd07427e6455e5588a3f748f4b168391b8c'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
