0301ba0a30dd49bc529d5cf3ada1ef016543ba0f
Anthony Goubard <anthony.goubard@japplis.com>
2024-04-24 13:14:41 +0200

    update VR Photo Converter 4.8

--- vr-photo-converter/tools/chocolateyinstall.ps1
+++ vr-photo-converter/tools/chocolateyinstall.ps1
@@ -3,15 +3,15 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'VRPhotoConverter.exe'
 
 $packageArgs = @{
-  packageName   = 'vr-photo-converter'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'VR Photo Converter*'
   checksum      = '8c414aaf904ffe2ce579c7bc0861dee91c4ec8e5e488b0ff50c3fb2c7411aca0'
   checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
