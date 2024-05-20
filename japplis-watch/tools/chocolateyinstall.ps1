7bc9a33b56b965f6b557200fe5bed73d20d24ac1
Anthony Goubard <anthony.goubard@japplis.com>
2024-05-20 11:07:05 +0200

    Japplis Watch version 1.9

--- japplis-watch/tools/chocolateyinstall.ps1
+++ japplis-watch/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'Watch.exe'
 
 $packageArgs = @{
-  packageName   = 'watch'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Japplis Watch*'
-  checksum      = '1d39d2fb58eeabca5235073f999c0c6d9ebfed29fd77df8ba8f46ce94036a2ca'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
