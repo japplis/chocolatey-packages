f5e8d20ed62e8bec900ad621acbfeab5aee35b39
Anthony Goubard <anthony.goubard@japplis.com>
2024-05-20 11:17:00 +0200

    Japplis Watch Pro version 1.9

--- japplis-watch-pro/tools/chocolateyinstall.ps1
+++ japplis-watch-pro/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'WatchPro.exe'
 
 $packageArgs = @{
-  packageName   = 'watch-pro'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Japplis Watch Pro*'
-  checksum      = 'b031db6a7f5bbf9c83c2926f0602be21402f36d5ad1f8c79de4fb688b89760ad'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
