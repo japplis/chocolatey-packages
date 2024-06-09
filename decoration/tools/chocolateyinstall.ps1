61bbacb44c52e2f5570221775546e58cd5f32d3d
Anthony Goubard <anthony.goubard@japplis.com>
2024-06-09 14:14:30 +0200

    Decoration version 5.4

--- decoration/tools/chocolateyinstall.ps1
+++ decoration/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'Decoration.exe'
 
 $packageArgs = @{
-  packageName   = 'decoration'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Decoration*'
-  checksum      = '05a1b00a370ecb6783b0f6ebd6dd13fea4e82835b3de8e3cb024af5379386a8e'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
