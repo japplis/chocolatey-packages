62308695f9936f2ca8ad31975f1321c1c5ba0353
Anthony Goubard <anthony.goubard@japplis.com>
2024-06-09 14:28:15 +0200

    Poster Font version 1.2

--- poster-font/tools/chocolateyinstall.ps1
+++ poster-font/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'PosterFont.exe'
 
 $packageArgs = @{
-  packageName   = 'poster-font'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Poster Font*'
-  checksum      = '9ce0b0e6bb61f6075b5a17b9102431bf452481b623a32e1eb1508e4bc04a034a'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
