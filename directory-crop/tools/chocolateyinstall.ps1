d01973dca1de33ecc98d07ae3e407d8ce702816e
Anthony Goubard <anthony.goubard@japplis.com>
2024-07-05 15:04:02 +0200

    Directory Crop version 1.2

--- directory-crop/tools/chocolateyinstall.ps1
+++ directory-crop/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'DirectoryCrop.exe'
 
 $packageArgs = @{
-  packageName   = 'directory-crop'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Directory Crop*'
-  checksum      = 'fba7dc3ab76adb42bf17f6f424ec606a05ae67fd5516566dc5d50ffca35b6931'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
