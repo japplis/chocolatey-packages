460fa7f11747a983cde2072007adf86e3ba9029e
Anthony Goubard <anthony.goubard@japplis.com>
2024-07-05 12:27:47 +0200

    Screenshot Crop version 1.2

--- screenshot-crop/tools/chocolateyinstall.ps1
+++ screenshot-crop/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'ScreenshotCrop.exe'
 
 $packageArgs = @{
-  packageName   = 'screenshot-crop'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Screenshot Crop*'
-  checksum      = 'b129942ee8bdd667bc003f5c142e38eb57512b2a442566f57b5a86316e62580a'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
