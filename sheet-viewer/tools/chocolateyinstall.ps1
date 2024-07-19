95abcfbf8db56b849a46b2070f2e72e52e5992c8
Anthony Goubard <anthony.goubard@japplis.com>
2024-07-19 10:20:29 +0200

    Sheet Viewer version 1.2

--- sheet-viewer/tools/chocolateyinstall.ps1
+++ sheet-viewer/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'SheetViewer.exe'
 
 $packageArgs = @{
-  packageName   = 'sheet-viewer'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Sheet Viewer*'
-  checksum      = 'b5134c39dd8d8a7cabefe3be2d1a21708c90f6670943152fe00e479a2bd826bd'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
