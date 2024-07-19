8ce156b9b209e120bf62d9604ffa5f1d63acbd43
Anthony Goubard <anthony.goubard@japplis.com>
2024-07-19 10:53:08 +0200

    Treecell version 1.3

--- treecell/tools/chocolateyinstall.ps1
+++ treecell/tools/chocolateyinstall.ps1
@@ -3,13 +3,11 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'Treecell.exe'
 
 $packageArgs = @{
-  packageName   = 'treecell'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Treecell*'
-  checksum      = '72ffa9324ac121982e3679aaa7d0924c9b4d00271cc8d3263bf18e70e5179a4e'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0, 1641, 3010)
 }
