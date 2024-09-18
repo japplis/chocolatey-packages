7ba58df3d283d266a47439edf01cdc8936793670
Anthony Goubard <anthony.goubard@japplis.com>
2024-09-18 14:52:48 +0200

    The Desktop Watchmaker version 3.3

--- the-desktop-watchmaker/tools/chocolateyinstall.ps1
+++ the-desktop-watchmaker/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'DesktopWatchmaker.exe'
 
 $packageArgs = @{
-  packageName   = 'desktop-watchmaker'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'The Desktop Watchmaker*'
-  checksum      = '87270c0e2181a4ec86a066f37f9301518b60ef8c2a81fb22b6d3c948e6a51b03'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
