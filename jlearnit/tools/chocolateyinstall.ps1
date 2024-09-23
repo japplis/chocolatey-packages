8ae61dabc0ee7ce0f53db02951d4aac5c1908c9b
Anthony Goubard <anthony.goubard@japplis.com>
2024-09-23 15:49:49 +0200

    JLearnIt version 6.7

--- jlearnit/tools/chocolateyinstall.ps1
+++ jlearnit/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'JLearnIt.exe'
 
 $packageArgs = @{
-  packageName   = 'jlearnit'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'JLearnIt*'
-  checksum      = '926bd9099b8166084835636bce1021a3eec05fbd9abe41a7d1ac0420166f006b'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
