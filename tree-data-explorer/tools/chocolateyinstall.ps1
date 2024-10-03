38ab08dd76e813066b73f43068c1c56c14ac2389
Anthony Goubard <anthony.goubard@japplis.com>
2024-10-03 17:12:44 +0200

    Tree Data Explorer version 1.2

--- tree-data-explorer/tools/chocolateyinstall.ps1
+++ tree-data-explorer/tools/chocolateyinstall.ps1
@@ -3,15 +3,13 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'TreeDataExplorer.exe'
 
 $packageArgs = @{
-  packageName   = 'tree-data-explorer'
+  packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Tree Data Explorer*'
-  checksum      = 'f1e8a86d39d08ae40508c9d29f660c2b4db7a32a060ebc60eb3def26af2458fa'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
