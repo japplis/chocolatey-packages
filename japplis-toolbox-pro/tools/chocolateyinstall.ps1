8417aad522c4c445767e8d1bacbcf46d1b0725cc
Anthony Goubard <anthony.goubard@japplis.com>
2024-10-18 18:07:33 +0200

    Japplis Toolbox Pro version 5.8

--- japplis-toolbox-pro/tools/chocolateyinstall.ps1
+++ japplis-toolbox-pro/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'ToolboxPro.exe'
 
 $packageArgs = @{
-  packageName   = 'toolbox-pro'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Japplis Toolbox Pro*'
-  checksum      = '9cef4e9929ce2cdd54fc46476ce42bdf3faf25374b3d2f3922ea07f34d2a6626'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
