e0ff36c3ef9976bdbeafddf5d3754d3928a35457
Anthony Goubard <anthony.goubard@japplis.com>
2024-10-21 10:18:21 +0200

    Decoration version 5.5

--- decoration/tools/chocolateyinstall.ps1
+++ decoration/tools/chocolateyinstall.ps1
@@ -4,7 +4,6 @@ $fileLocation = Join-Path $toolsDir 'Decoration.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Decoration*'
