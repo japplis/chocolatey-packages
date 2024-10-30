15fd9bc75fee3316d99a143d62d788db4beca49a
Anthony Goubard <anthony.goubard@japplis.com>
2024-10-30 13:47:58 +0100

    Ant Commander Pro version 4.15

--- ant-commander-pro/tools/chocolateyinstall.ps1
+++ ant-commander-pro/tools/chocolateyinstall.ps1
@@ -4,7 +4,6 @@ $fileLocation = Join-Path $toolsDir 'AntCommanderPro.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Ant Commander Pro*'
