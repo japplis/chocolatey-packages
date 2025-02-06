d6bc513235b0cae59259c2ddbb1f75839611d006
Anthony Goubard <anthony.goubard@japplis.com>
2025-02-06 10:53:44 +0100

    Poster Font Light version 1.2

--- poster-font-light/tools/chocolateyinstall.ps1
+++ poster-font-light/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'PosterFontLight.exe'
 
 $packageArgs = @{
-  packageName   = 'poster-font-light'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Poster Font Light*'
-  checksum      = 'b544cea915855dd84b11aaa65f8a5293bf9f5d023963d5ffb11f3fc7bfbc7b0a'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
