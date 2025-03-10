1607c2330861d0a6b85382c641dffb38d028a801
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-10 10:34:24 +0100

    Post On Screen version 1.3

--- post-on-screen/tools/chocolateyinstall.ps1
+++ post-on-screen/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'PostOnScreen.exe'
 
 $packageArgs = @{
-  packageName   = 'post-on-screen'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Post On Screen*'
-  checksum      = '1fd12a7d580a608bdaaf601dd280dfc59da46fd57952194c3749a134eef310e1'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
