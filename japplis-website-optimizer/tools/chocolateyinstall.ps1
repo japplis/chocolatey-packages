102ad0ef482d8df29143d3cba4563f23d67cbe04
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-10 10:01:01 +0100

    Japplis Website Optimizer version 4.3

--- japplis-website-optimizer/tools/chocolateyinstall.ps1
+++ japplis-website-optimizer/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'WebsiteOptimizer.exe'
 
 $packageArgs = @{
-  packageName   = 'website-optimizer'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Japplis Website Optimizer*'
-  checksum      = 'b0ccc9329d491df5e66e55ee39d2ce20f549d51ac0d956be3ad45ec7ad005983'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
