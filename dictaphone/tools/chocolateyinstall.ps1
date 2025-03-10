16f538aff91378cc06dc20bf56a2cc08a82b0614
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-10 10:17:37 +0100

    Dictaphone version 1.2

--- dictaphone/tools/chocolateyinstall.ps1
+++ dictaphone/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'Dictaphone.exe'
 
 $packageArgs = @{
-  packageName   = 'dictaphone'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Dictaphone*'
-  checksum      = '883f20f91fbfe0265b307ac50481adb086f337f8aca38a4972fe51c96e965a58'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
