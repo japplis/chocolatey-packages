0345ccc851970589b34d4229b31a5dd622e7b14d
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-10 15:41:31 +0100

    Japplis Watch Pro version 1.10

--- japplis-watch-pro/tools/chocolateyinstall.ps1
+++ japplis-watch-pro/tools/chocolateyinstall.ps1
@@ -4,7 +4,6 @@ $fileLocation = Join-Path $toolsDir 'WatchPro.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Japplis Watch Pro*'
