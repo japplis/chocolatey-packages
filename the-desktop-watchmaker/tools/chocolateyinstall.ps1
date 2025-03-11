1da011dabe478d235a39b3e1808f24eea35535e6
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-11 16:28:01 +0100

    The Desktop Watchmaker version 3.4

--- the-desktop-watchmaker/tools/chocolateyinstall.ps1
+++ the-desktop-watchmaker/tools/chocolateyinstall.ps1
@@ -4,7 +4,6 @@ $fileLocation = Join-Path $toolsDir 'DesktopWatchmaker.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'The Desktop Watchmaker*'
