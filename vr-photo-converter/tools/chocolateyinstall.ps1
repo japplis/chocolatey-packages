ebc4f623b05b177cd2fe32fcef4f14baf2762d82
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-11 09:53:51 +0100

    VR Photo Converter version 2.3

--- vr-photo-converter/tools/chocolateyinstall.ps1
+++ vr-photo-converter/tools/chocolateyinstall.ps1
@@ -4,12 +4,9 @@ $fileLocation = Join-Path $toolsDir 'VRPhotoConverter.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
   fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'VR Photo Converter*'
-  checksum      = '8c414aaf904ffe2ce579c7bc0861dee91c4ec8e5e488b0ff50c3fb2c7411aca0'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
   validExitCodes= @(0, 1641, 3010)
 }
