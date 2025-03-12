4328b49312f178c1ff03db8442627ef9f1de739e
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-12 10:25:33 +0100

    M2 Repo Cleaner version 1.1

--- m2-repo-cleaner/tools/chocolateyinstall.ps1
+++ m2-repo-cleaner/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'M2RepoCleaner.exe'
 
 $packageArgs = @{
-  packageName   = 'm2-repo-cleaner'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'M2 Repo Cleaner*'
-  checksum      = 'a05ca11eaca247d01700832f35c0ba56ac1b3d361d3dcc239e7321173571eeec'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
