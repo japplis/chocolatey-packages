af03dde2c4920610d4ac39018cb161c992323d07
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-12 14:23:56 +0100

    Japplis Pastel version 1.3

--- japplis-pastel/tools/chocolateyinstall.ps1
+++ japplis-pastel/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'Pastel.exe'
 
 $packageArgs = @{
-  packageName   = 'pastel'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Japplis Pastel*'
-  checksum      = '54486387ae6ea6abd92decc093650e56e00547e26e99f782bc184be691b95a05'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
