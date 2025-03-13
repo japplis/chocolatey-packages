ad7f463380b0900d0d2cdc2951658444b753a0b0
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-13 10:35:03 +0100

    Japplis Clipboard History version 1.2

--- japplis-clipboard-history/tools/chocolateyinstall.ps1
+++ japplis-clipboard-history/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'ClipboardHistory.exe'
 
 $packageArgs = @{
-  packageName   = 'clipboard-history'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Japplis Clipboard History*'
-  checksum      = '46e9c318fc5d996b7adb32b7dd44fa1839e8eb5b6ca6398635e5da00249a98d8'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
