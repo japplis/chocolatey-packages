00d4c6f4c5cd3c37ba64373f7c13bfb4d3eac55d
Anthony Goubard <anthony.goubard@japplis.com>
2025-03-13 10:59:49 +0100

    Control Dashboard version 1.2

--- control-dashboard/tools/chocolateyinstall.ps1
+++ control-dashboard/tools/chocolateyinstall.ps1
@@ -3,15 +3,12 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'ControlDashboard.exe'
 
 $packageArgs = @{
-  packageName   = 'control-dashboard'
-  unzipLocation = $toolsDir
-  fileType      = 'EXE'
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'exe'
   file          = $fileLocation
   softwareName  = 'Control Dashboard*'
-  checksum      = 'c4658b9944600b5a602035d5b51c30fd24f05a3766eeda939f05e0d07823ef5a'
-  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /LOG=`"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).InnoInstall.log`""
-  validExitCodes= @(0,3010,1641)
+  validExitCodes= @(0, 1641, 3010)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
